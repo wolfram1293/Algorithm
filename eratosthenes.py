f6e452dbfcd4dcedccb1a45d594881378d887e1d
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2021-02-10 00:29:54 +0900

    eratosthenes

---
+++ eratosthenes.py
@@ -0,0 +1,12 @@
+def eratosthenes(n):
+    prime=[2]
+    lim=int(n**0.5)
+    d=[i+1 for i in range(2,n,2)]
+    while True:
+        p=d[0]
+        if lim<p:
+            prime=prime+d
+            break
+        prime.append(p)
+        d=[x for x in d if x%p!=0]
+    return prime
