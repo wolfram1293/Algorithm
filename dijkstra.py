3de7dc6db45130782383f7f211b5626d606411e8
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2021-08-19 03:36:02 +0900

    add BellmanFord

--- dijkstra.py
+++ dijkstra.py
@@ -1,7 +1,7 @@
 import sys
 import heapq
 
-#edges=[[]for i in range(N)]
+#edges = [[]for i in range(N)]
 #edges[a].append([b,d])
 edges = [[[1, 5], [2, 4]], #ノードA0
 [[0, 5], [3, 3], [5, 9]], #ノードB1
@@ -12,29 +12,29 @@ edges = [[[1, 5], [2, 4]], #ノードA0
 [[3, 7], [4, 8], [5, 2], [7, 2]], #ノードG6
 [[5, 5], [6, 2]]] #ノードH7
 
-N=len(edges)
-S=0
+N = len(edges)
+S = 0
 
-def dijkstra(N,S,edges):
-    done=[False]*N
-    inf=10**9
-    dist=[inf]*N
-    dist[S]=0
-    heap=[]
+def Dijkstra(N,S,edges):
+    done = [False]*N
+    inf = 10**9
+    dist = [inf]*N
+    dist[S] = 0
+    heap = []
     heapq.heappush(heap,[dist[S],S])
     while heap:
         node=heapq.heappop(heap)[1]
         if not done[node]:
             for e in edges[node]:
-                if dist[e[0]]>dist[node]+e[1]:
-                    dist[e[0]]=dist[node]+e[1]
+                if dist[e[0]] > dist[node]+e[1]:
+                    dist[e[0]] = dist[node]+e[1]
                     heapq.heappush(heap,[dist[e[0]],e[0]])
-        done[node]=True
+        done[node] = True
 
     for i in range(N):
-        if dist[i]==inf:
-            dist[i]='INF'
+        if dist[i] == inf:
+            dist[i] = 'INF'
     
     print(' '.join(map(str, dist)))
 
-dijkstra(N,S,edges)
+Dijkstra(N,S,edges)
