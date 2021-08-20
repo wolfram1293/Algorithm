35df5c57333f12bca1f92608b6d75bf56953afb0
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2021-08-20 23:05:08 +0900

    add min heap

--- heap.py
+++ heap.py
@@ -1,4 +1,53 @@
+# 配列を用いたヒープの実装 iに対して親(i-1)//2,左の子2i+1, 右の子2i+2となる
+
 # 最小ヒープ
+class minHeap:
+    def __init__(self):
+        self.list=[]
+
+    def heappush(self,a: int):
+        self.list.append(a)
+        index = len(self.list) - 1
+        while 1:
+            parent = (index-1) // 2
+            if index == 0:
+                break
+            if self.list[index] < self.list[parent]:
+                self.list[index],self.list[parent] = self.list[parent],self.list[index]
+                index = parent
+            else: 
+                break         
+
+    def heappop(self):
+        a = self.list[0]
+        self.list[0] = self.list.pop()
+        parent = 0        
+        while 1:
+            c1 = 2*parent + 1
+            c2 = 2*parent + 2
+            if c2 >= len(self.list):
+                c = c1
+            else:
+                if self.list[c1] > self.list[c2]:
+                    c = c2
+                else:
+                    c = c1
+            if c >= len(self.list):
+                break
+                
+            if self.list[c] < self.list[parent]:
+                self.list[c],self.list[parent] = self.list[parent],self.list[c]
+                parent = c
+            else:
+                break
+        return a
+
+heap = minHeap()
+heap.heappush(5)
+heap.heappush(3)
+heap.heappush(4)
+heap.heappop()
+
 
 # 最大ヒープ
 class maxHeap:
@@ -25,7 +74,7 @@ class maxHeap:
         while 1:
             c1 = 2*parent + 1
             c2 = 2*parent + 2
-            if c2 > len(self.list) - 1:
+            if c2 >= len(self.list):
                 c = c1
             else:
                 if self.list[c1] < self.list[c2]:
