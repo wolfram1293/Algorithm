6f1491a94beb1a0b47c951c2bc39fbf64cc0d953
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2021-08-20 13:49:30 +0900

    add uf

--- Union-Find.py
+++ Union-Find.py
@@ -1,7 +1,9 @@
-#根の親の値にサイズの情報を持たせる
+# 根の親の値にサイズの情報を持たせる 要素が根（ルート）の場合は-(そのグループの要素数)を格納する (根の親の値にランク（木の高さ）の情報を保持してもいい)
+# 汎用バージョン 参考 https://note.nkmk.me/python-union-find/
+
 class UnionFind():
     def __init__(self, n):
-        self.parents = [-1] * n
+        self.parent = [-1] * n
 
     def unite(self, x, y):
         x = self.find(x)
@@ -10,42 +12,38 @@ class UnionFind():
         if x == y:
             return
 
-        if self.parents[x] > self.parents[y]:
+        if -self.parent[y] > -self.parent[x]:
             x, y = y, x
 
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
+        self.parent[x] += self.parent[y]
+        self.parent[y] = x
     
-    def find(self, x):
-        if self.parents[x] < 0:
+    def find(self, x): # 要素xが属するグループの根を返す
+        if self.parent[x] < 0:
             return x
         else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+            self.parent[x] = self.find(self.parent[x]) # 経路圧縮 根を調べる際に、調べた要素の親を根に変更しつなぎ直す
+            return self.parent[x]
 
-    def size(self, x):
-        return -self.parents[self.find(x)]
+    def size(self, x): # 要素xが属するグループの要素数
+        return -self.parent[self.find(x)]
 
-    def same(self, x, y):
+    def same(self, x, y): # 同じグループに属するかどうかを返す
         return self.find(x) == self.find(y)
 
-    def members(self, x):
+    def members(self, x): # 要素xが属するグループに属する要素をリストで返す
         root = self.find(x)
         return [i for i in range(self.n) if self.find(i) == root]
 
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
+    def roots(self): # すべての根の要素をリストで返す
+        return [i for i, x in enumerate(self.parent) if x < 0]
 
-    def group_count(self):
+    def group_count(self): # グループの数を返す
         return len(self.roots())
 
-    def all_group_members(self):
-        return {r: self.members(r) for r in self.roots()}
 
-    def __str__(self):
-        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
-
-
+# 文字列やタプル((0, 0), )などを要素とする場合
+# 文字列と番号のペアの辞書を用意
 l = ['A', 'B', 'C', 'D', 'E']
 
 d = {x: i for i, x in enumerate(l)}
@@ -90,13 +88,13 @@ print(uf_s.group_count())
 # 2
 
 
-
+# 元のクラスを継承したクラス。__init__()で2つの辞書を生成して入出力時に使う。
 class UnionFindLabel(UnionFind):
     def __init__(self, labels):
         assert len(labels) == len(set(labels))
 
         self.n = len(labels)
-        self.parents = [-1] * self.n
+        self.parent = [-1] * self.n
         self.d = {x: i for i, x in enumerate(labels)}
         self.d_inv = {i: x for i, x in enumerate(labels)}
 
@@ -117,6 +115,42 @@ class UnionFindLabel(UnionFind):
         return [self.d_inv[i] for i in range(self.n) if self.find(i) == root]
 
     def roots(self):
-        return [self.d_inv[i] for i, x in enumerate(self.parents) if x < 0]
+        return [self.d_inv[i] for i, x in enumerate(self.parent) if x < 0]
 
 
+l = ['A', 'B', 'C', 'D', 'E']
+ufl = UnionFindLabel(l)
+print(ufl)
+# A: ['A']
+# B: ['B']
+# C: ['C']
+# D: ['D']
+# E: ['E']
+
+ufl.union('A', 'D')
+ufl.union('D', 'C')
+ufl.union('E', 'B')
+print(ufl)
+# A: ['A', 'C', 'D']
+# E: ['B', 'E']
+
+print(ufl.find_label('D'))
+# A
+
+print(ufl.size('D'))
+# 3
+
+print(ufl.same('A', 'D'))
+# True
+
+print(ufl.members('D'))
+# ['A', 'C', 'D']
+
+print(ufl.roots())
+# ['A', 'E']
+
+print(ufl.group_count())
+# 2
+
+print(ufl.all_group_members())
+# {'A': ['A', 'C', 'D'], 'E': ['B', 'E']}
