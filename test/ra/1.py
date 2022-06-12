19e962f480a1c3f69759291329ecbe82d23f413a
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2022-06-12 22:22:36 +0900

    add ra

---
+++ test/ra/1.py
@@ -0,0 +1,71 @@
+def solution(A, B, C):
+    # write your code in Python 3.6
+    l = [[A,'a'], [B,'b'], [C,'c']]
+    l.sort(reverse=True)
+    ans = ''
+    if l[0][0] // 2 + 1 > l[1][0] + l[2][0]:
+        while True:
+            for i in range(2):
+                if l[0][0] == 0:
+                    break
+                ans += l[0][1]
+                l[0][0] -= 1
+            
+            if l[1][0] > 0:
+                ans += l[1][1]
+                l[1][0] -= 1
+                continue
+
+            elif l[2][0] > 0:
+                ans += l[2][1]
+                l[2][0] -= 1
+                continue
+
+            else:
+                break
+
+    elif l[0][0] // 2 + 1  > l[1][0] // 2 + 1  + l[2][0]:
+        while True:
+            for i in range(2):
+                if l[0][0] == 0:
+                    break
+                ans += l[0][1]
+                l[0][0] -= 1
+
+            for i in range(2):
+                if l[1][0] == 0:
+                    break
+                ans += l[1][1]
+                l[1][0] -= 1
+            
+            if l[2][0] > 0:
+                ans += l[2][1]
+                l[2][0] -= 1
+                continue
+
+            else:
+                break
+    
+    elif l[0][0] > l[1][0] + l[2][0]:
+        while True:
+            for i in range(1):
+                if l[0][0] == 0:
+                    break
+                ans += l[0][1]
+                l[0][0] -= 1
+            
+            if l[1][0] > 0:
+                ans += l[1][1]
+                l[1][0] -= 1
+                continue
+
+            elif l[2][0] > 0:
+                ans += l[2][1]
+                l[2][0] -= 1
+                continue
+
+            else:
+                break
+
+
+    return ans
