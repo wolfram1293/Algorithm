31e0cda64214dbac2829ddb72d47bd9893aed4e2
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2022-06-12 22:43:09 +0900

    update ra comment

--- test/ra/0.py
+++ test/ra/0.py
@@ -1,7 +1,7 @@
 def solution(A):
     # write your code in Python 3.6
     A.sort()
-    N = len(A)
+    N = len(A) # いらんかった
     ans = 1
     for a in A:
         if a == ans:
@@ -10,3 +10,5 @@ def solution(A):
             break
             
     return ans
+
+# 1以上の整数で配列の中にない最小のものを探す 多分これが一番簡潔
