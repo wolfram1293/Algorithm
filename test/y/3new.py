2e1bc4d2cdd921b5b1434eafd01ead82be14371a
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2022-06-16 00:52:43 +0900

    update 3

--- test/y/3new.py
+++ test/y/3new.py
@@ -1,4 +1,3 @@
-import sys
 def seven(day):
     day = list(str(day))
     day = [int(v) for v in day]
@@ -7,11 +6,9 @@ def seven(day):
     else:
         return False
 
-def cntseven(day):
-    ans = 0
+def fuga(day):    
     day = list(str(day))
     day = [int(v) for v in day]
-    flag = False
     hoge = [0] * len(day)
     digit = 0
     for i in range(len(day)):
@@ -19,6 +16,14 @@ def cntseven(day):
             hoge[-(i+1)] = int(10**(digit-1)) + hoge[-i] * 9
 
         digit += 1
+    return hoge
+
+def cntseven(day):
+    ans = 0
+    hoge = fuga(day)
+    day = list(str(day))
+    day = [int(v) for v in day]
+    flag = False
     
     digit = len(day)
     for i in range(len(day)):
@@ -27,6 +32,7 @@ def cntseven(day):
 
         if flag:
             ans += int(''.join(map(str, day[i:]))) + 1
+            break
         elif i == len(day) - 1:
             if d >= 7:
                 ans += 1
@@ -43,17 +49,35 @@ def cntseven(day):
 
 a, b, n = map(int, input().split())
 point = 0
-day = 0
+day0 = n // a
+hoge = fuga(day0)
+ratio = hoge[0] / 10 ** (len(list(str(hoge[0]))))
 
-day1 = n // a
+day = int(n / (a + b * ratio))
 
-while True:
-    point += a
-    if seven(day):
-        point += b
-    if point >= n:
-        break
+point = a * (day + 1) + b * cntseven(day)
+if point < n:
+    print('a')
+    while True:
+        day += 1
+        point += a
+        if seven(day):
+            point += b
+        if point >= n:
+            print(point)
+            break
 
-    day += 1
+else:
+    print('b')
+    while True:
+        day -= 1
+        point -= a
+        if seven(day):
+            point -= b
+        if point == n:
+            break
+        elif point < n:
+            day += 1
+            break
 
 print(day)
